8ca50ae21397d9aade9a577441f4a2ea1fd895b5
V-Sarayu <159168805+V-Sarayu@users.noreply.github.com>
2024-05-19 12:24:09 +0530

    Update customer.c

--- customer.c
+++ customer.c
@@ -3,7 +3,6 @@
 
 // Function to input customer details and write to file
 void inputCustomerDetails(struct Customer *customer) {
-    // Input customer details
     printf("Enter customer details:\n");
     printf("Name: ");
     scanf("%s", customer->name);
