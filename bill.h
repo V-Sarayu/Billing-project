b6ce22e928748fefdf56405f9a130b82c496d7e3
V-Sarayu <159168805+V-Sarayu@users.noreply.github.com>
2024-05-19 13:23:55 +0530

    Update bill.h

--- bill.h
+++ bill.h
@@ -1,7 +1,21 @@
 #ifndef BILL_H
 #define BILL_H
 
-// Function to generate a bill
+#define MAX_PRODUCTS 100
+
+struct Bill {
+    int number;
+    // Add any other bill-related fields here as needed
+};
+
+typedef struct {
+    char name[100];
+    float price;
+    int quantity;
+} StockItem;
+
 void generateBill();
+void viewStock();
+void updateStock();
 
 #endif /* BILL_H */
